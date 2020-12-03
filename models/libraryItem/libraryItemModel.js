02c95c0ded901fbcbdb6cdd594a44485def13319
john lundgren <john.lundgren.lexicon@gmail.com>
2020-12-03 15:58:37 +0100

    Removal of LibraryItem id in Category model.

--- models/libraryItem/libraryItemModel.js
+++ models/libraryItem/libraryItemModel.js
@@ -27,7 +27,8 @@ exports.addLibraryItem = async (req, res, next) => {
     try{
         const itemToAdd = Object.assign({ categoryId: req.params.categoryId }, req.body);
         const libraryItem = await LibraryItem.create(itemToAdd);
-        const result = await Category.findByIdAndUpdate(req.params.categoryId, {
+        // Mandatory to add the library item id into the Category model to be able to use populate method in Category model.
+        await Category.findByIdAndUpdate(req.params.categoryId, {
             $push: { libraryItems: libraryItem._id }
         }, { safe: true, upsert: true});
         res.status(201).send(libraryItem);
@@ -42,6 +43,8 @@ exports.deleteLibraryItem = async (req, res, next) => {
         if(!deletedItem){
             return res.status(422).send({errorMessage: `No library item with id: ${req.params.id} was found`})
         }
+        // Must even remove the id from the Category model.
+        await Category.findByIdAndUpdate(req.params.categoryId, {"$pull": { libraryItems: deletedItem._id }})
         res.status(200).send(deletedItem);
     } catch(error){
         next(error);
